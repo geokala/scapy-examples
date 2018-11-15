44d7677a89f35f6d94520f30860f51e7168ed9cb
Madda <nobody.youknow@local>
2018-11-15 18:41:43 +0000

    Update scapy version

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ from sys import version
 
 requirements = [
     'netifaces',
+    'scapy',
 ]
 if int(version[0]) == 3:
-    requirements.append('scapy-python3')
     requirements.append('pyx')  # For showing PDF packets
 else:
-    requirements.append('scapy')
     requirements.append('pyx==0.12.1')  # For showing PDF packets
 
 setup(
     name="scapy_examples",
-    version="0.4.1",
+    version="0.4.2",
     install_requires=requirements,
 )
